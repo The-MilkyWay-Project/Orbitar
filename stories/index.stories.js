e6f30d05b7f65d11f108f514315e0ac450266293
Josh <joshua.walker@sykescottages.co.uk>
2020-02-23 19:50:06 +0000

    first commit

---
+++ stories/index.stories.js
@@ -0,0 +1,12 @@
+import { action } from '@storybook/addon-actions';
+
+import Nav from 'kepler-4b';
+
+export default {
+  title: 'Navigation',
+  component: Nav,
+};
+
+export const Navigation = () => ({
+  Component: Nav,
+});
